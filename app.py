b65faca4e4a1875902b788fe4dada6ba4da4c947
62random <josepedropinto98@gmail.com>
2017-09-24 17:34:24 +0100

    only @robb

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["confirma98"]:
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -39,8 +39,7 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    if message_text == "@robb":
-                        send_message(sender_id, "roger that!")
+                    send_message(sender_id, "roger that!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
@@ -59,7 +58,7 @@ def send_message(recipient_id, message_text):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
-        "access_token": os.environ["EAAG0YnX4dJoBAETo5hYxZCuFShgFFresiwXZCMPWhcN2wqFrEZBP0o1KEa9dwWZAI00ScqMMXZBeqp6TxS5bVISzNhOdwhh8UZAp0dseFYzkgo1aCkyZBK0RYT1xIid11albPCqpW0hLJlXgzqD7YzwlAlVIaNDRA3bdw5JmDFMerD66trb9ZBguBKo81DgoiKsZD"]
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
     }
     headers = {
         "Content-Type": "application/json"
