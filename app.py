5849cf9c8ed6f281c86a2225cba6d60de1313706
62random <josepedropinto98@gmail.com>
2017-09-25 00:22:24 +0100

    only @robb

--- app.py
+++ app.py
@@ -38,10 +38,8 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    if message_text[:6] ==  "@robb ":
                             try:
-                                send_message(sender_id, str(eval(message_text[6:])))
+                                send_message(sender_id, str(eval(message_text)))
                             except:
                                 send_message(sender_id, "enche 10")
 
