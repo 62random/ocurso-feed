2386d59cc8cff812700f17c3cb1fbd39d5dbadd4
62random <josepedropinto98@gmail.com>
2017-09-25 00:56:49 +0100

    only @robb

--- app.py
+++ app.py
@@ -38,10 +38,12 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-                    try:
-                        send_message(sender_id, str(eval(message_text)))
-                    except:
-                        send_message(sender_id, "enche 10")
+
+                if message_text != "/":
+                        try:
+                            send_message(sender_id, str(eval(message_text)))
+                        except:
+                            send_message(sender_id, "Olá Ângela :)")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
