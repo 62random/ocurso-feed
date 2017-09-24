07f766dfba7a9d35690c8d8aa08229b2837accfc
62random <josepedropinto98@gmail.com>
2017-09-24 22:11:22 +0100

    only @robb

--- app.py
+++ app.py
@@ -39,8 +39,8 @@ def webhook():
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
 
-                    if 'message_text'[:6] == "@robb ":
-                        send_message(sender_id, ("https://www.google.pt/search?q=" ++ 'message_text'[6:] ++ "\nEnche aí 10 paneleiro!!"))
+                    if message_text == "@robb":
+                        send_message(sender_id, "roger that!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
