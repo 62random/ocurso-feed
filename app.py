e9d36e9aeebe6d5d49ecfb85ccb3a5126d16e845
62random <josepedropinto98@gmail.com>
2017-09-25 01:15:11 +0100

    only @robb

--- app.py
+++ app.py
@@ -31,29 +31,31 @@ def webhook():
     if data["object"] == "page":
 
         for entry in data["entry"]:
-            for messaging_event in entry["messaging"]:
+            try:
+                for messaging_event in entry["messaging"]:
 
-                if messaging_event.get("message"):  # someone sent us a message
+                    if messaging_event.get("message"):  # someone sent us a message
 
-                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
+                        sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                        recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                        message_text = messaging_event["message"]["text"]  # the message's text
 
-                if message_text != "/":
-                        try:
-                            send_message(sender_id, str(eval(message_text)))
-                        except:
-                            send_message(sender_id, "enche 10")
+                    if message_text != "/":
+                            try:
+                                send_message(sender_id, str(eval(message_text)))
+                            except:
+                                send_message(sender_id, "enche 10")
 
-                if messaging_event.get("delivery"):  # delivery confirmation
-                    pass
+                    if messaging_event.get("delivery"):  # delivery confirmation
+                        pass
 
-                if messaging_event.get("optin"):  # optin confirmation
-                    pass
+                    if messaging_event.get("optin"):  # optin confirmation
+                        pass
 
-                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-                    pass
-
+                    if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                        pass
+            except:
+                pass
     return "ok", 200
 
 
