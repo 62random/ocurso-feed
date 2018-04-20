337b06c62d43b1710764b4cecc26c095f2f065d3
62random <josepedropinto98@gmail.com>
2018-04-20 16:27:56 +0100

    lele

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def created_post(data):
 
 	string = "New reply from user <" + sender_id + "> on topic \"" + title + "\"\n@" + time + "\nAnd said: \n\"" + said + "\""
 
-	send_message(CONST_ID, sender_id)
+	send_message(CONST_ID, title)
 
 
 def created_topic(data):
