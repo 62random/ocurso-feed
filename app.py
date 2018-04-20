70d3ca6a8d7e750d166efb03f919c7d6f7b889b2
62random <josepedropinto98@gmail.com>
2018-04-20 10:40:09 +0100

    ola

--- app.py
+++ app.py
@@ -23,13 +23,13 @@ def verify():
 
 @app.route('/', methods=['POST'])
 def webhook():
+	# endpoint for processing incoming messaging events
+
+	data = request.get_json()
+	log(data)  # you may not want to log every incoming message in production, but it's good for testing
 	send_message(CONST_ID, "olá")
 	return "ok", 200
 '''
-    # endpoint for processing incoming messaging events
-
-    data = request.get_json()
-    log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
     if data["object"] == "page":
 
