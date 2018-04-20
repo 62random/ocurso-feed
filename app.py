3f77d79fe495f47d2781d3ffb304e36b21051315
62random <josepedropinto98@gmail.com>
2018-04-20 10:13:11 +0100

    lalal

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def verify():
 @app.route('/', methods=['POST'])
 def webhook():
 
-    # endpoint for processing incoming messaging events
+    # endpoint for processing incoming messaging events lel
 
     data = request.get_json()
     log(data)  # you may not want to log every incoming message in production, but it's good for testing
