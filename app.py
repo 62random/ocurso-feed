6d3ed134a7decedd336ea8c55c6f618fa2f82620
62random <josepedropinto98@gmail.com>
2018-04-23 16:02:19 +0100

    bloco implementado

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from flask import Flask, request
 app = Flask(__name__)
 
 RANDOM = 1471279112955772
-BLOCO = {RANDOM}
+BLOCO = [RANDOM, RANDOM, RANDOM]
 TAG_RE = re.compile(r'<[^>]+>')
 
 @app.route('/', methods=['GET'])
