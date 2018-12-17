e335d84401328f1a50caba35cb71c967ffe05254
62random <josepedropinto98@gmail.com>
2018-12-17 18:44:22 +0000

    stack

--- app.py
+++ app.py
@@ -31,13 +31,12 @@ def webhook():
     # endpoint for processing incoming messaging events
 	data = request.get_json()
 	log(data)  # you may not want to log every incoming message in production, but it's good for testing
+	try:
+		stack(data)
+	except:
+		pass
 
-    try:
-        stack(data)
-    except:
-        pass
-
-    try:
+	try:
 		facebook_message(data)
 		return "ok", 200
 	except:
